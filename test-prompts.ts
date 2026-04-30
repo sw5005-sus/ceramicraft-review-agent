b91eae4163350923e83c1e36fbeff66798062c13
lyric777 <wysxxuzhili48@163.com>
2026-04-30 22:32:57 +0800

    clean test

--- test-prompts.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import { registerSystemPrompts } from "./src/utils/mlflowClient.js";
-
-console.log("Registering prompts to MLflow...");
-await registerSystemPrompts();
-console.log("Done.");
